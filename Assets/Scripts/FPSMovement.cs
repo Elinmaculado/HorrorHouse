12cc8a406c3c985c2289cfe65b810a09709d9bb8
Miguel239g <miguel.ruiz@uniat.edu.mx>
2025-11-03 12:40:18 -0600

    Character controller

---
+++ Assets/Scripts/FPSMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FPSMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private PlayerInputs playerInput;
+
+    public float movementSpeed = 3;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = new PlayerInputs();
+        playerInput.Player.Enable();
+
+    }
+        // Update is called once per frame
+    void Update() 
+    {
+        PlayerMovement();    
+    }
+
+    private void PlayerMovement()
+    {
+        Transform camTransform = Camera.main.transform;
+        Vector3 camForward = camTransform.forward;
+        Vector3 camRight = camTransform.right;
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
+        
+        
+        
+        
+        var movementInput = playerInput.Player.Move.ReadValue<Vector2>();
+        Vector3 movement = camForward * movementInput.y + camRight * movementInput.x;
+        controller.Move(movement * movementSpeed * Time.deltaTime);
+    }
+}
